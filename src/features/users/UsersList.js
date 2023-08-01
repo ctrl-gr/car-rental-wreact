b54aaf691df778335621f82a9d79eaf5d4f082fc
ctrl-gr <giulia@giuliaredolfi.it>
2023-08-01 12:06:06 +0200

    get users

--- src/features/users/UsersList.js
+++ src/features/users/UsersList.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import {
     useGetUsersQuery,
     useAddNewUserMutation,
     useUpdateUserMutation,
     useDeleteUserMutation
 } from "../api/apiSlice";
+import Table from "../../components/table/core/Table";
 
 function UsersList() {
     const [addNewUser, response] = useAddNewUserMutation()
@@ -23,7 +24,7 @@ function UsersList() {
         }))
     }
 
-    const [updatePost, {isLoading: isUpdating }] = useUpdateUserMutation()
+    const [updatePost, {isLoading: isUpdating}] = useUpdateUserMutation()
     const setPostData = (data) => {
         setInputField({
             firstName: data.firstName,
@@ -49,7 +50,7 @@ function UsersList() {
 
     const onSubmit = (e) => {
         e.preventDefault()
-        const {firstName, lastName, username, password } = e.target.elements
+        const {firstName, lastName, username, password} = e.target.elements
         setInputField((inputField) => ({
             ...inputField,
             [e.target.name]: e.target.value,
@@ -81,100 +82,113 @@ function UsersList() {
         isSuccess: isGetSuccess,
         isError: isGetError,
         error: getError
-    } = useGetUsersQuery({ refetchOnMountOrArgChange: true})
+    } = useGetUsersQuery({refetchOnMountOrArgChange: true})
 
 
-let usersContent
-if (isGetLoading) {
-    usersContent = (
-        <div className="d-flex justify-content-center">
-            <div className="spinner-border" role="status">
-                <span className="visually-hidden">Loading...</span>
-            </div>
-        </div>
-    )
-} else if (isGetSuccess) {
-    usersContent = users.map((user) => {
-        return (
-            <div className="col-lg-12 mb-3" key={user.id}>
-                <div className="card alert alert-secondary">
-                    <div className="card-body">
-                        <h5 className="card-title">{user.username}</h5>
-                        <p className="card-text">{user.firstName}</p>
-                        <p className="card-text">{user.lastName}</p>
-                        <button
-                            onClick={() => deleteUser(user.id)}
-                            className="btn btn-outline-danger me-2"
-                        >
-                            Remove
-                        </button>
-                        <button
-                            onClick={() => setPostData(user)}
-                            className="btn btn-outline-primary"
-                        >
-                            Edit
-                        </button>
-                    </div>
+    let usersContent
+    const usersListHeaders = ['firstName', 'lastName', 'username', 'azioni']
+    const usersListActions = [
+        {
+            type: 'modifica',
+            actionOnTop: false,
+            cssClass: ''
+        },
+        {
+            type: 'elimina',
+            actionOnTop: false,
+            cssClass: ''
+        },
+        {
+            type: 'nuovo',
+            actionOnTop: true,
+            cssClass: ''
+        }
+    ]
+
+    function actionEmitter(type, valueToEmit) {
+        switch (type) {
+            case 'modifica':
+                return console.log(valueToEmit)
+                // return setPostData(valueToEmit)
+            case 'elimina':
+                return (deleteUser)
+            case 'nuovo':
+                return console.log('nuovo elemento')
+            default:
+                return console.log('actions clicked', valueToEmit)
+        }
+    }
+
+
+    if (isGetLoading) {
+        usersContent = (
+            <div className="d-flex justify-content-center">
+                <div className="spinner-border" role="status">
+                    <span className="visually-hidden">Loading...</span>
                 </div>
             </div>
         )
-    })
-} else if (isGetError) {
-    usersContent = (
-        <div className="alert alert-danger" role="alert">
-            {getError}
+    } else if (isGetSuccess) {
+        return (
+            <Table headers={usersListHeaders} data={users} actions={usersListActions} handleAction={actionEmitter}/>
+        )
+    } else if (isGetError) {
+        usersContent = (
+            <div className="alert alert-danger" role="alert">
+                {getError}
+            </div>
+        )
+    }
+    return (
+        <div className="row">
+            <div className="col-md-4 offset-md-*">
+                <form onSubmit={onSubmit}>
+                    <div className="mb-3">
+                        <label className="form-label">
+                            <strong>Enter Title</strong>
+                        </label>
+                        <input
+                            value={inputField.firstName}
+                            type="text"
+                            className="form-control"
+                            name="title"
+                            id="title"
+                            onChange={inputsHandler}
+                        />
+                        <input
+                            value={inputField.lastName}
+                            type="text"
+                            className="form-control"
+                            name="title"
+                            id="title"
+                            onChange={inputsHandler}
+                        />
+                        <input
+                            value={inputField.username}
+                            type="text"
+                            className="form-control"
+                            name="title"
+                            id="title"
+                            onChange={inputsHandler}
+                        />
+                    </div>
+                    <button className="btn btn-danger me-2" type="submit">
+                        Submit
+                    </button>
+                    <button
+                        onClick={onEditData}
+                        className="btn btn-primary"
+                        type="button"
+                    >
+                        Update
+                    </button>
+                </form>
+            </div>
+            <div className="col-lg-8">
+                <div className="row">{usersContent}</div>
+            </div>
         </div>
     )
 }
-return (
-    <div className="row">
-        <div className="col-md-4 offset-md-*">
-            <form onSubmit={onSubmit}>
-                <div className="mb-3">
-                    <label className="form-label">
-                        <strong>Enter Title</strong>
-                    </label>
-                    <input
-                        value={inputField.firstName}
-                        type="text"
-                        className="form-control"
-                        name="title"
-                        id="title"
-                        onChange={inputsHandler}
-                    />
-                    <input
-                        value={inputField.lastName}
-                        type="text"
-                        className="form-control"
-                        name="title"
-                        id="title"
-                        onChange={inputsHandler}
-                    />
-                    <input
-                        value={inputField.username}
-                        type="text"
-                        className="form-control"
-                        name="title"
-                        id="title"
-                        onChange={inputsHandler}
-                    />
-                </div>
-                <button className="btn btn-danger me-2" type="submit">
-                    Submit
-                </button>
-                <button
-                    onClick={onEditData}
-                    className="btn btn-primary"
-                    type="button"
-                >
-                    Update
-                </button>
-            </form>
-        </div>
-        <div className="col-lg-8">
-            <div className="row">{usersContent}</div>
-        </div>
-    </div>
-)
-}
+
 export default UsersList
